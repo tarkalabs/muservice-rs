73a60a3175e80114d14447f586e59e62d0a950a9
Dhruva Sagar <dhruva.sagar@gmail.com>
2021-06-09 22:49:40 +0530

    Add api to create user

--- src/router.rs
+++ src/router.rs
@@ -2,6 +2,7 @@ use hyper::{
     Body,
     Request,
     Response,
+    body::to_bytes
 };
 use routerify::{Router, Middleware};
 use routerify::prelude::*;
@@ -25,6 +26,29 @@ async fn users_handler(req: Request<Body>) -> Result<Response<Body>> {
     Ok(resp)
 }
 
+async fn create_user_handler(mut req: Request<Body>) -> Result<Response<Body>> {
+    let body = req.body_mut();
+    let user_bytes = to_bytes(body).await?;
+    let mut user = serde_json::from_slice::<User>(&user_bytes).context("Unable to parse user")?;
+    let state = req.data::<AppState>().unwrap();
+    match user.insert(&state.db().connection()).await {
+        Ok(()) => {
+            let res = Response::builder()
+                .header(hyper::header::CONTENT_TYPE, "application/json")
+                .status(hyper::StatusCode::CREATED)
+                .body(serde_json::to_string(&user).unwrap().into())?;
+            Ok(res)
+        },
+        Err(e) => {
+            let res = Response::builder()
+                .header(hyper::header::CONTENT_TYPE, "application/json")
+                .status(hyper::StatusCode::UNPROCESSABLE_ENTITY)
+                .body(serde_json::to_string(&e.to_string()).unwrap().into())?;
+            Ok(res)
+        },
+    }
+}
+
 async fn logger(req: Request<Body>) -> Result<Request<Body>> {
     info!("{} {} {}", req.remote_addr(), req.method(), req.uri().path());
     Ok(req)
@@ -36,6 +60,7 @@ pub async fn build_router() -> Result<Router<Body, Error>> {
         .data(state)
         .middleware(Middleware::pre(logger))
         .get("/users", users_handler)
+        .post("/users", create_user_handler)
         .get("/", home_handler)
         .build().unwrap();
     Ok(router)
