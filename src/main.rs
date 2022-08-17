3903e20c21c9abc86e5cfa99e2f728f1848762a1
sbeauli2 <sbeauli2@unb.ca>
2022-08-17 14:33:14 -0300

    WIP3

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use anyhow::{Result, Context};
-use tracing::subscriber::set_global_default;
-use tracing_subscriber::FmtSubscriber;
+// use tracing::subscriber::set_global_default;
+// use tracing_subscriber::FmtSubscriber;
 use libmuservice::{router, server};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let subscribe = FmtSubscriber::new();
-    set_global_default(subscribe).context("Unable to setup fmt subscriber")?;
+    //Old code for subscriber
+    // let subscribe = FmtSubscriber::new();
+    // set_global_default(subscribe).context("Unable to setup fmt subscriber")?;
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "muservice=debug,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     print!("RUST_LOG: {:?}\n", std::env::var("RUST_LOG").context("No RUST_LOG"));
 
